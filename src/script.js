ce21dd60a2fa54b5670e4bde9ad0959325272b1e
tisha <Valeri.Vashkevich@yandex.by>
2021-08-06 19:58:52 +0300

    create card-container

--- src/script.js
+++ src/script.js
@@ -8,11 +8,19 @@ container.className = 'container'
 const header = document.createElement('header')
 header.className = 'header'
 
+const main = document.createElement('main')
+main.className = 'main'
+
+const footer = document.createElement('footer')
+footer.className = 'footer'
+
 const burgerMenu = document.createElement('div')
 burgerMenu.className = 'burger-menu'
 
 app.append(container)
 container.append(header)
+container.append(main)
+container.append(footer)
 
 header.append(burgerMenu)
 for (let i = 0; i < 3; i++) {
@@ -20,3 +28,12 @@ for (let i = 0; i < 3; i++) {
     lineBurger.className = 'line_burger-menu'
     burgerMenu.append(lineBurger)
 }
+
+for (let i = 0; i < 8; i++) {
+    const cardContainer = document.createElement('div')
+    cardContainer.className = 'card-container'
+    main.append(cardContainer)
+}
+
+
+
